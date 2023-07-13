255d5e6390a394dd0785b8ef76125d6d7d710ff3
Kevin Adrian <adrian.kevin@yahoo.com>
2023-07-13 17:29:15 +0800

    add payment with stripe

--- src/PlansScreen.js
+++ src/PlansScreen.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./css/PlansScreen.css";
 import db from "./firebase";
 import {
+  addDoc,
   collection,
   doc,
+  getDoc,
   getDocs,
+  onSnapshot,
   query,
-  setDoc,
   where,
 } from "firebase/firestore";
 import { useSelector } from "react-redux";
@@ -14,7 +16,23 @@ import { selectUser } from "./features/userSlice";
 
 function PlanScreen() {
   const [products, setProducts] = useState({});
+  const [subscription, setSubscription] = useState();
+  const [buttonClicked, setButtonClicked]= useState("");
   const user = useSelector(selectUser);
+
+
+  useEffect(()=>{
+    async function fetchData(){
+      const subscriptionSnapshot = await getDocs(
+        collection(db, "customers", user.uid, "subscriptions")
+      );
+      subscriptionSnapshot.forEach(async (subscriptionDoc)=>{
+        const subscriptionSnap = await getDoc(doc(db,"customers",user.uid,"subscriptions",subscriptionDoc.id));
+        setSubscription(subscriptionSnap.data().role);
+      })
+    }
+    fetchData();
+  },[]);
 
   useEffect(() => {
     async function fetchData() {
@@ -37,33 +55,35 @@ function PlanScreen() {
 
       setProducts(temp);
     }
+
     fetchData();
   }, []);
 
   const loadCheckout = async function (priceId) {
-    const customerDoc = doc(db, "customers", user.uid,"checkout_sessions");
-    const docRef = await setDoc(customerDoc, {
+    const docRef = await addDoc(collection(db,"customers", user.uid, "checkout_sessions"), {
       price: priceId,
       success_url: window.location.origin,
       cancel_url: window.location.origin,
     });
-    
-    docRef.onSnapshot(customerDoc, (snap) => {
+    onSnapshot(docRef, (snap) => {
       const { error, url } = snap.data();
       if (error) {
+        // Show an error to your customer and
+        // inspect your Cloud Function logs in the Firebase console.
         alert(`An error occured: ${error.message}`);
       }
       if (url) {
+        // We have a Stripe Checkout URL, let's redirect.
         window.location.assign(url);
       }
-    });
+    })
   };
 
   return (
     <div className="plansScreen">
       {Object.entries(products).map(([productId, productData]) => {
         return (
-          <div className="planScreen_plan" key={productId}>
+          <div className="planScreen_plan" key={productId} id={productData.role}>
             <div className="plansScreen_info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
@@ -72,8 +92,9 @@ function PlanScreen() {
               onClick={() => {
                 loadCheckout(productData.prices.priceId);
               }}
+              className={subscription === productData.role ? "planbtn_disabled": "planbtn_active"}
             >
-              Subscribe
+              {subscription === productData.role ? "Current Plan": "Subscribe"}
             </button>
           </div>
         );
